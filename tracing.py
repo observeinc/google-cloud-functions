98c0a272b0cecbd7bd5e9fb7082842d1fb49384b
Luke Zhu <luke@observeinc.com>
2023-01-10 21:04:15 -0800

    fix: fix cloud scheduler job and kind urls (#3)
    This change makes it possible for us to model the Cloud Scheduler Job data
    
    This change also fixes some spans, catches pubsub large message exceptions, and introduces a couple of minor env variables.

--- tracing.py
+++ tracing.py
@@ -1,12 +1,19 @@
+import os
+
 from opentelemetry import trace
 from opentelemetry.sdk import trace as sdktrace
 from opentelemetry.sdk.trace import export
 
+disable_logging = os.getenv("DISABLE_LOGGING")
+
 provider = sdktrace.TracerProvider()
-processor = export.BatchSpanProcessor(
-    # Set indent to none to avoid multi-line logs
-    export.ConsoleSpanExporter(formatter=lambda s: s.to_json(indent=None) + "\n")
-)
-provider.add_span_processor(processor)
-trace.set_tracer_provider(provider)
+
+if disable_logging is None: 
+    _processor = export.BatchSpanProcessor(
+        # Set indent to none to avoid multi-line logs
+        export.ConsoleSpanExporter(formatter=lambda s: s.to_json(indent=None) + "\n")
+    )
+    provider.add_span_processor(_processor)
+    trace.set_tracer_provider(provider)
+
 tracer = trace.get_tracer(__name__)
