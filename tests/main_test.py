dd67c5e8c3ffacfae95c6d824c9ebd7278a60cba
Colin Hutchinson <colin.hutchinson@observeinc.com>
2023-07-04 10:12:59 -0400

    chore(ci): add the unit tests to CI, setup dependabot, setup pre-commit

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 import unittest
 from unittest.mock import patch, MagicMock
 from main import export_assets
 
+
 class TestExportAssets(unittest.TestCase):
-    @patch('main.asset_v1')
-    @patch('main.asset_v1.OutputConfig')
-    @patch('main.asset_v1.ExportAssetsRequest')
+    @patch("main.asset_v1")
+    @patch("main.asset_v1.OutputConfig")
+    @patch("main.asset_v1.ExportAssetsRequest")
     def test_export_assets(self, mock_request, mock_output_config, mock_asset_v1):
         mock_client = MagicMock()
         mock_asset_v1.AssetServiceClient.return_value = mock_client
@@ -13,7 +15,7 @@ class TestExportAssets(unittest.TestCase):
         mock_request = MagicMock()
         request_data = {
             "asset_types": ["storage.googleapis.com.*"],
-            "content_types": ["RESOURCE"]
+            "content_types": ["RESOURCE"],
         }
         mock_request.get_json.return_value = request_data
 
@@ -23,10 +25,12 @@ class TestExportAssets(unittest.TestCase):
         mock_client.export_assets.assert_called()
 
     # Test case for when request doesn't contain "asset_types" and/or "content_type"
-    @patch('main.asset_v1')
-    @patch('main.asset_v1.OutputConfig')
-    @patch('main.asset_v1.ExportAssetsRequest')
-    def test_export_assets_no_asset_or_content_types(self, mock_request, mock_output_config, mock_asset_v1):
+    @patch("main.asset_v1")
+    @patch("main.asset_v1.OutputConfig")
+    @patch("main.asset_v1.ExportAssetsRequest")
+    def test_export_assets_no_asset_or_content_types(
+        self, mock_request, mock_output_config, mock_asset_v1
+    ):
         mock_client = MagicMock()
         mock_asset_v1.AssetServiceClient.return_value = mock_client
 
@@ -40,17 +44,19 @@ class TestExportAssets(unittest.TestCase):
         mock_client.export_assets.assert_called()
 
     # Test case for when an invalid content type is provided
-    @patch('main.asset_v1')
-    @patch('main.asset_v1.OutputConfig')
-    @patch('main.asset_v1.ExportAssetsRequest')
-    def test_export_assets_invalid_content_type(self, mock_request, mock_output_config, mock_asset_v1):
+    @patch("main.asset_v1")
+    @patch("main.asset_v1.OutputConfig")
+    @patch("main.asset_v1.ExportAssetsRequest")
+    def test_export_assets_invalid_content_type(
+        self, mock_request, mock_output_config, mock_asset_v1
+    ):
         mock_client = MagicMock()
         mock_asset_v1.AssetServiceClient.return_value = mock_client
 
         mock_request = MagicMock()
         request_data = {
             "asset_types": ["storage.googleapis.com.*"],
-            "content_type": ["INVALID"]
+            "content_type": ["INVALID"],
         }
         mock_request.get_json.return_value = request_data
 
@@ -58,10 +64,12 @@ class TestExportAssets(unittest.TestCase):
             export_assets(mock_request)
 
     # Test case for when `export_assets` in the Google Cloud SDK throws an exception
-    @patch('main.asset_v1')
-    @patch('main.asset_v1.OutputConfig')
-    @patch('main.asset_v1.ExportAssetsRequest')
-    def test_export_assets_sdk_exception(self, mock_request, mock_output_config, mock_asset_v1):
+    @patch("main.asset_v1")
+    @patch("main.asset_v1.OutputConfig")
+    @patch("main.asset_v1.ExportAssetsRequest")
+    def test_export_assets_sdk_exception(
+        self, mock_request, mock_output_config, mock_asset_v1
+    ):
         mock_client = MagicMock()
         mock_client.export_assets.side_effect = Exception("SDK error")
         mock_asset_v1.AssetServiceClient.return_value = mock_client
@@ -69,10 +77,12 @@ class TestExportAssets(unittest.TestCase):
         mock_request = MagicMock()
         request_data = {
             "asset_types": ["storage.googleapis.com.*"],
-            "content_type": ["RESOURCE"]
+            "content_type": ["RESOURCE"],
         }
         mock_request.get_json.return_value = request_data
 
         response = export_assets(mock_request)
 
-        self.assertEqual(response, ("Failed to export content type RESOURCE. Error: SDK error", 500))
+        self.assertEqual(
+            response, ("Failed to export content type RESOURCE. Error: SDK error", 500)
+        )
