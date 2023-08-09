26a335bb7c81a9aaafc2bb2f9ef707290f26faed
Colin Hutchinson <colin.hutchinson@observeinc.com>
2023-08-09 11:48:32 -0400

    chore(tests): updated the test when asset type and content type aren't defined

--- tests/main_test.py
+++ tests/main_test.py
@@ -52,23 +52,48 @@ class TestExportAssets(unittest.TestCase):
         self.assertTrue(args[0].endswith("/operation_name.txt"))
 
     # Test case for when request doesn't contain "asset_types" and/or "content_type"
+    @patch("main.create_cloud_task")
+    @patch("main.storage")
     @patch("main.asset_v1")
     @patch("main.asset_v1.OutputConfig")
     @patch("main.asset_v1.ExportAssetsRequest")
     def test_export_assets_no_asset_or_content_types(
-        self, mock_request, mock_output_config, mock_asset_v1
+        self,
+        mock_request,
+        mock_output_config,
+        mock_asset_v1,
+        mock_storage,
+        mock_create_cloud_task,
     ):
         mock_client = MagicMock()
         mock_asset_v1.AssetServiceClient.return_value = mock_client
 
+        mock_operation = MagicMock()
+        mock_operation.operation.name = "mocked_operation_name"
+        mock_client.export_assets.return_value = mock_operation
+
         mock_request = MagicMock()
-        request_data = {"some_other_field": "some_value"}
+        request_data = {}
         mock_request.get_json.return_value = request_data
+
+        mock_bucket = MagicMock()
+        mock_blob = MagicMock()
+        mock_storage.Client.return_value.bucket.return_value = mock_bucket
+        mock_bucket.blob.return_value = mock_blob
 
         response = export_assets(mock_request)
 
+        # Asserting the function behavior
         self.assertEqual(response, ("Asset export triggered", 200))
         mock_client.export_assets.assert_called()
+
+        # Check if create_cloud_task was called
+        self.assertEqual(mock_create_cloud_task.call_count, 2)
+
+        # Get the first set of arguments passed to create_cloud_task
+        args, _ = mock_create_cloud_task.call_args
+        self.assertTrue(args[0].startswith("bucket_placeholder/asset_export_v2_"))
+        self.assertTrue(args[0].endswith("/operation_name.txt"))
 
     # Test case for when an invalid content type is provided
     @patch("main.asset_v1")
